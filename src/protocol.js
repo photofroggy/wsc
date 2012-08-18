b46dcb69a38718c0721b0056af151eb7c38e971a
Henry Rapley <froggywillneverdie@msn.com>
2012-08-18 04:15:14 +0100

    implemented autorejoin.

--- src/protocol.js
+++ src/protocol.js
@@ -122,7 +122,7 @@ function wsc_protocol( client ) {
             client.bind('pkt.login', this.login);
             client.bind('pkt.join', this.join);
             client.bind('pkt.part', this.part);
-            //client.bind('pkt.kicked', this.kicked);
+            client.bind('pkt.kicked', this.kicked);
             client.bind('pkt.ping', this.ping);
             client.bind('pkt.property', this.property);
             client.bind('pkt.recv_join', this.recv_joinpart);
@@ -281,7 +281,6 @@ function wsc_protocol( client ) {
                 return;
             msg = msgm[0];
             
-            console.log(event);
             if( event.s == '0' ) {
                 return;
             }
@@ -395,6 +394,14 @@ function wsc_protocol( client ) {
                 c.server_message("Couldn't leave "+ns, e.e);
             }
             
+        },
+        
+        kicked: function( e, client ) {
+        
+            if( e.r.toLowerCase().indexOf('autokicked') > -1 )
+                return;
+            client.join(e.ns);
+        
         },
         
         // Process a property packet.
