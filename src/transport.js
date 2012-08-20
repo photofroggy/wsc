65fef7928369440894abfc79fdcc949c17465f4f
Henry Rapley <froggywillneverdie@msn.com>
2012-08-20 11:11:38 +0100

    more comments. brush up api

--- src/transport.js
+++ src/transport.js
@@ -4,39 +4,117 @@
  * 
  * @class Transport
  * @constructor
+ * @param server {String} Address for the server to connect to.
+ * @param [open=wsc.Transport.sopen] {Method} This method will be called when
+ *   a connection is established with the server.
+ * @param [message=wsc.Transport.smessage] {Method} When a message is received
+ *   on the transport, this method will be called.
+ * @param [disconnect=wsc.Transport.sdisconnect] {Method} The method to be
+ *   called when the connection has been closed.
  */
-wsc.Transport = function( open, message, disconnect ) {
+wsc.Transport = function( server, open, message, disconnect ) {
 
+    this.sock = null;
     this.conn = null;
+    this.server = server;
     this.open( open );
     this.message( message );
     this.disconnect( disconnect );
 
 };
 
+/**
+ * Static class method.
+ * Create a new client transport object.
+ * 
+ * @method Create
+ */
+wsc.Transport.Create = function(  ) {};
+
+/**
+ * Register open event callback.
+ * 
+ * @method open
+ * @param [callback=wsc.Transport.sopen] {Method} This method will be called
+ *   when a connection is established with the server.
+ */
 wsc.Transport.prototype.open = function( callback ) {
 
     this._open = callback || this.sopen;
 
 };
 
+/**
+ * Register message event callback.
+ * 
+ * @method message
+ * @param [callback=wsc.Transport.smessage] {Method} When a message is received
+ *   on the transport, this method will be called.
+ */
 wsc.Transport.prototype.message = function( callback ) {
 
     this._message = callback || this.smessage;
 
 };
 
+/**
+ * Register disconnect event callback.
+ * 
+ * @method disconnect
+ * @param [callback=wsc.Transport.sdisconnect] {Method} The method to be called
+ *   when the connection has been closed.
+ */
 wsc.Transport.prototype.disconnect = function( callback ) {
 
     this._disconnect = callback || this.sdisconnect;
 
 };
 
+/**
+ * Open connection event stub.
+ * 
+ * @method sopen
+ */
 wsc.Transport.prototype.sopen = function(  ) {};
+
+/**
+ * Message event stub.
+ * 
+ * @method smessage
+ */
 wsc.Transport.prototype.smessage = function(  ) {};
+
+/**
+ * Close connection event stub.
+ * 
+ * @method sdisconnect
+ */
 wsc.Transport.prototype.sdisconnect = function(  ) {};
 
-wsc.Transport.prototype.send = function( data ) {};
+wsc.Transport.prototype._open = function( event, sock ) {};
+wsc.Transport.prototype._message = function( event ) {};
+wsc.Transport.prototype._disconnect = function( event ) {};
+
+/**
+ * Connect to the server.
+ * 
+ * @method connect
+ */
+wsc.Transport.prototype.connect = function(  ) {};
+
+/**
+ * Send a message to the server.
+ * 
+ * @method send
+ * @param message {String} message to send to the server.
+ */
+wsc.Transport.prototype.send = function( message ) {};
+
+/**
+ * Close the connection.
+ * 
+ * @method close
+ */
 wsc.Transport.prototype.close = function(  ) {};
 
 
