3fe39b07cd742925df7cc8f0677c668bda6e57d7
Henry Rapley <froggywillneverdie@msn.com>
2012-08-20 17:41:04 +0100

    basic implementation done.

--- src/flow.js
+++ src/flow.js
@@ -1,5 +1,5 @@
 /**
- * Control the client's program flow in relation to the chat protocol.
+ * Control the client's program flow in relation to the chat this.
  * 
  * @class Flow
  * @constructor
@@ -25,107 +25,107 @@ wsc.Flow.prototype.handle = function( event, client ) {
 };
 
 // Respond to pings from the server.
-wsc.Flow.prototype.ping: function( event, client ) {
+wsc.Flow.prototype.ping = function( event, client ) {
     client.pong();
 };
 
 // Respond to a llama-style handshake.
-wsc.Flow.prototype.chatserver: function ( e ) {
-    //protocol.client.monitor(
-    //    "Connected to " + e.pkt["cmd"] + " " + e.pkt["arg"]["server"] + " version " +e.pkt["arg"]["version"]+".");
-    protocol.client.login();
+wsc.Flow.prototype.chatserver = function ( e ) {
+    //client.monitor(
+    //    "Connected to " + event.pkt["cmd"] + " " + event.pkt["arg"]["server"] + " version " +e.pkt["arg"]["version"]+".");
+    client.login();
 };
 
 // Process a login packet
-wsc.Flow.prototype.login: function( e ) {
+wsc.Flow.prototype.login = function( event, client ) {
     
     if(e.pkt["arg"]["e"] == "ok") {
-        //protocol.client.monitor("Logged in as " + e.pkt["param"] + '.');
+        //client.monitor("Logged in as " + event.pkt["param"] + '.');
         // Use the username returned by the server!
-        info = new WscPacket('info\n' + e.data);
-        protocol.client.settings["username"] = e.pkt["param"];
-        protocol.client.settings['symbol'] = info.arg.symbol;
-        protocol.client.settings['userinfo'] = info.arg;
+        info = new wsc.Packet('info\n' + event.data);
+        client.settings["username"] = event.pkt["param"];
+        client.settings['symbol'] = info.arg.symbol;
+        client.settings['userinfo'] = info.arg;
         // Autojoin!
         // TODO: multi-channel?
-        if ( protocol.client.fresh )
-            protocol.client.join(client.settings["autojoin"]);
+        if ( client.fresh )
+            client.join(client.settings["autojoin"]);
         else {
-            for( key in protocol.client.channelo ) {
-                if( protocol.client.channelo[key].info['namespace'][0] != '~' )
-                    protocol.client.join(key);
+            for( key in client.channelo ) {
+                if( client.channelo[key].info['namespace'][0] != '~' )
+                    client.join(key);
             }
         }
     } else {
-        //protocol.client.monitor("Failed to log in: \"" + e.pkt["arg"]["e"] + '"');
+        //client.close();
     }
     
-    if( protocol.client.fresh )
-        protocol.client.fresh = false;
+    if( client.fresh )
+        client.fresh = false;
     
     
 };
 
 // Received a join packet.
-wsc.Flow.prototype.join: function( e ) {
+wsc.Flow.prototype.join = function( event, client ) {
     if(e.pkt["arg"]["e"] == "ok") {
-        ns = protocol.client.deform_ns(e.pkt["param"]);
-        //protocol.client.monitor("You have joined " + ns + '.');
-        protocol.client.create_channel(ns);
-        protocol.client.ui.channel(ns).server_message("You have joined " + ns);
+        ns = client.deform_ns(e.pkt["param"]);
+        //client.monitor("You have joined " + ns + '.');
+        client.create_channel(ns);
+        client.ui.channel(ns).server_message("You have joined " + ns);
     } else {
-        protocol.client.ui.chatbook.current.server_message("Failed to join " + protocol.client.deform_ns(e.pkt["param"]), e.pkt["arg"]["e"]);
+        client.ui.chatbook.current.server_message("Failed to join " + client.deform_ns(e.pkt["param"]), event.pkt["arg"]["e"]);
     }
 };
 
 // Received a part packet.
-wsc.Flow.prototype.part: function( e ) {
-    ns = protocol.client.deform_ns(e.ns);
-    c = protocol.client.channel(ns);
+wsc.Flow.prototype.part = function( event, client ) {
+    ns = client.deform_ns(e.ns);
+    c = client.channel(ns);
     
     if(e.e == "ok") {
         info = '';
-        if( e.r )
-            info = '[' + e.r + ']';
+        if( event.r )
+            info = '[' + event.r + ']';
         
         msg = 'You have left ' + ns;
         c.server_message(msg, info);
         
         if( info == '' )
-            protocol.client.remove_channel(ns);
+            client.remove_channel(ns);
         
-        if( protocol.client.channels() == 0 ) {
-            switch( e.r ) {
+        if( client.channels() == 0 ) {
+            switch( event.r ) {
                 case 'bad data':
                 case 'bad msg':
                 case 'msg too big':
                     break;
                 default:
-                    if( e.r.indexOf('killed:') < 0 )
+                    if( event.r.indexOf('killed:') < 0 )
                         return;
                     break;
             }
-            protocol.process_data( { data: 'disconnect\ne='+e.r+'\n' } );
+            this.process_data( { data: 'disconnect\ne='+e.r+'\n' } );
         }
     } else {
-        protocol.client.monitor('Couldn\'t leave ' + ns, e.e);
-        c.server_message("Couldn't leave "+ns, e.e);
+        client.monitor('Couldn\'t leave ' + ns, event.e);
+        c.server_message("Couldn't leave "+ns, event.e);
     }
     
 };
 
-wsc.Flow.prototype.kicked: function( e, client ) {
+wsc.Flow.prototype.kicked = function( e, client ) {
 
-    if( e.r.toLowerCase().indexOf('autokicked') > -1 )
+    if( event.r.toLowerCase().indexOf('autokicked') > -1 )
         return;
     client.join(e.ns);
 
 };
 
 // Process a property packet.
-wsc.Flow.prototype.property: function( e ) {
+wsc.Flow.prototype.property = function( event, client ) {
     //console.log(e.pkt["arg"]["p"]);
-    chan = protocol.client.channel(e.pkt["param"]);
+    chan = client.channel(e.pkt["param"]);
     
     if( !chan )
         return;
@@ -134,27 +134,30 @@ wsc.Flow.prototype.property: function( e ) {
 };
 
 // User join or part.
-wsc.Flow.prototype.recv_joinpart: function( e ) {
-    c = protocol.client.channel(e.ns);
-    if( e.name == 'recv_join')
+wsc.Flow.prototype.recv_joinpart = function( event, client ) {
+    c = client.channel(e.ns);
+    if( event.name == 'recv_join')
         c.recv_join(e);
     else
         c.recv_part(e);
 };
 
+wsc.Flow.prototype.recv_join = wsc.Flow.prototype.recv_joinpart;
+wsc.Flow.prototype.recv_part = wsc.Flow.prototype.recv_joinpart;
+
 // Display a message received from a channel.
-wsc.Flow.prototype.recv_msg: function( e ) {
-    protocol.client.channel(e.ns).recv_msg(e);
+wsc.Flow.prototype.recv_msg = function( event, client ) {
+    client.channel(e.ns).recv_msg(e);
 };
 
 // Someone was promoted or demoted.
-wsc.Flow.prototype.recv_privchg: function( e ) {
-    protocol.client.channel(e.ns).recv_privchg(e);
+wsc.Flow.prototype.recv_privchg = function( event, client ) {
+    client.channel(e.ns).recv_privchg(e);
 };
 
 // Some sucka got kicked foo.
-wsc.Flow.prototype.recv_kicked: function( e ) {
-    protocol.client.channel(e.ns).recv_kicked(e);
+wsc.Flow.prototype.recv_kicked = function( event, client ) {
+    client.channel(e.ns).recv_kicked(e);
 };
 
 
