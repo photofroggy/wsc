7565fc65b3d0085f226f97068981b911db1e5a4b
Henry Rapley <froggywillneverdie@msn.com>
2012-09-02 12:39:11 +0100

    bundled dAmn extension in its own thingy

--- dist/wsc.dAmn.js
+++ dist/wsc.dAmn.js
@@ -4416,6 +4416,174 @@ Chatterbox.template.userinfo = '<div class="userinfo" id="{username}">\
                             </strong>\
                             <ul>{info}</ul></div>\
                         </div>';
+/**
+ * dAmn module lolol.
+ * 
+ * @module wsc.dAmn
+ */
+wsc.dAmn = {};
+wsc.dAmn.VERSION = '0.1.1';
+wsc.dAmn.STATE = 'alpha';
+
+/*
+ * This function returns a map which can be used by the tablumps parser to parse
+ * dAmn's tablumps.
+ */
+wsc.dAmn.Tablumps = function(  ) {
+    return {
+        '&avatar\t': [ 2,
+            ':icon{0}:',
+            function( data ) { return wsc.dAmn.avatar.link( data[0], data[1] ); }
+        ],
+        '&emote\t': [ 5,
+            '{0}',
+            '<img alt="{0}" width="{1}" height="{2}" title="{3}" src="http://e.deviantart.com/emoticons/{4}" />'
+        ],
+        '&dev\t': [ 2,
+            ':dev{1}:',
+            '{0}<a target="_blank" alt=":dev{1}:" href="http://{1}.deviantart.com/">{1}</a>',
+            '{0}\x1b[36m{1}\x1b[39m'
+        ],
+        '&thumb\t': [ 7,
+            ':thumb{0}:',
+            function( data ) {
+                id = data[0];
+                user = data[2];
+                dim = data[3].split('x'); w = parseInt(dim[0]); h = parseInt(dim[1]);
+                f = data[5];
+                flags = data[6].split(':');
+                lu = user.substring(1).replace(/^[^a-zA-Z0-9\-_]/, '');
+                // Deviation title.
+                t = data[1];
+                ut = (t.replace(/[^A-Za-z0-9]+/g, '-').replace(/^-+/, '').replace(/-+$/, '') || '-') + '-' + id;
+                // Deviation link tag. First segment only.
+                title = t + ' by ' + user + ', ' + w + 'x' + h;
+                dal = '<a target="_blank" href="http://' + lu + '.deviantart.com/art/' + ut + '" title="' + title + '"';
+                
+                // Time to go through the flags.
+                if( flags[1] != '0' )
+                    return dal + '>[mature deviation: ' + t + ']</a>';
+                
+                if( flags[2] != '0' )
+                    return dal + '>[deviation: ' + t + ']</a>';
+                
+                shadow = flags[0] == '0';
+                isgif = f.match( /\.gif$/i );
+                
+                if( isgif && ( w > 100 || h > 100 ) )
+                    return dal + '>[deviation: ' + t + ']</a>';
+                
+                server = parseInt(data[4]);
+                
+                if( w/h > 1) {
+                    th = parseInt((h * 100) / w);
+                    tw = 100;
+                } else {
+                    tw = parseInt((w * 100) / h);
+                    th = 100;
+                }
+                
+                if( tw > w || th > h ) {
+                    tw = w;
+                    th = h;
+                }
+                
+                if( isgif ) {
+                    f = f.replace(/:/, '/');
+                    path = 'http://fc0' + server + '.deviantart.net/' + f;
+                    det = f.split('/');
+                    if( det.length > 1 ) {
+                        det = det['.'];
+                        if( det && det.length > 2 )
+                            path = 'http://' + file;
+                    }
+                    return dal + '><img class="thumb" title="' + title +
+                        '" width="'+tw+'" height="'+th+'" alt=":thumb'+id+':" src="' + path +'" /></a>';
+                }
+                path = 'http://backend.deviantart.com/oembed?url=http://www.deviantart.com/deviation/'+id+'&format=thumb150';
+                
+                if( f.match(/.png$/i) )
+                    shadow = false;
+                
+                return dal + '><img class="thumb' + ( shadow ? ' shadow' : '' ) + '" width="'+tw+'" height="'+
+                    th+'" alt=":thumb'+id+':" src="'+path+'" /></a>';
+            }
+        ],
+    };
+
+};
+
+
+wsc.dAmn.Extension = function( client ) {
+
+    client.settings.client = 'dAmnClient';
+    client.settings.clientver = '0.3';
+    client.settings.domain = 'deviantart.com';
+    client.settings.agent+= ' wsc/dAmn/' + wsc.dAmn.VERSION;
+    
+    client.protocol.extend_maps({
+        'dAmnServer': ['version']
+    });
+    
+    client.protocol.extend_messages({
+        'dAmnServer': ['<span class="servermsg">** Connected to dAmnServer {version} *</span>', false, true ]
+    });
+    
+    client.protocol.tablumps.extend(wsc.dAmn.Tablumps());
+    
+    client.flow.dAmnServer = client.flow.chatserver;
+    
+    client.ui.on( 'userinfo.before', function( event, ui ) {
+        event.user.avatar = wsc.dAmn.avatar.link(event.user.name, event.user.member.usericon);
+        
+        if( event.user.member.realname )
+            event.user.info.push(event.user.member.realname);
+        
+        if( event.user.member.typename )
+            event.user.info.push(event.user.member.typename);
+    });
+    
+    client.ui.on('log_item.after',
+        function( event, ui ) {
+            userbox = event.item.find('span.u-photofroggy');
+            if( userbox.length == 0 )
+                return;
+            userbox.first().css('color', 'red');
+        }
+    );
+
+};
+
+wsc.dAmn.avatar = {};
+wsc.dAmn.avatar.ext = [ 'gif', 'gif', 'jpg', 'png' ];
+
+wsc.dAmn.avatar.link = function( un, icon ) {
+    icon = parseInt(icon);
+    cachebuster = (icon >> 2) & 15;
+    icon = icon & 3;
+    ext = wsc.dAmn.avatar.ext[icon] || 'gif';
+    
+    if (cachebuster) {
+        cachebuster = '?' + cachebuster;
+    }
+    else {
+        cachebuster = '';
+    }
+    
+    if( icon == 0 ) { 
+        ico = 'default';
+    } else {
+        ru = new RegExp('\\$un(\\[([0-9]+)\\])', 'g');
+        
+        ico = '$un[0]/$un[1]/{un}'.replace(ru, function ( m, s, i ) {
+            return un[i].toLowerCase();
+        });
+        ico = ico.replacePArg( '{un}', un.toLowerCase() );
+    }
+    
+    return '<a target="_blank" title=":icon'+un+':" href="http://'+un+'.deviantart.com/"><img class="avatar"\
+            alt=":icon'+un+':" src="http://a.deviantart.net/avatars/'+ico+'.'+ext+cachebuster+'" height="50" width="50" /></a>';
+};
 // @include templates.js
 // @include lib.js
 // @include packet.js
