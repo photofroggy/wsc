21df3e5e5f2c98969e26016662a22156c659f3b0
Henry Rapley <froggywillneverdie@msn.com>
2012-12-27 21:20:51 +0000

    bump.

--- src/base.js
+++ src/base.js
@@ -4,9 +4,9 @@
  * @module wsc
  */
 var wsc = {};
-wsc.VERSION = '1.0.3';
+wsc.VERSION = '1.0.4';
 wsc.STATE = 'release candidate';
-wsc.REVISION = '0.14.88';
+wsc.REVISION = '0.14.89';
 wsc.defaults = {};
 wsc.defaults.theme = 'wsct_default';
 wsc.defaults.themes = [ 'wsct_default', 'wsct_dAmn' ];
