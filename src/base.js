e1bdd9858b4310ebaf66b98529b2600da3ebac88
Henry Rapley <froggywillneverdie@msn.com>
2012-12-27 03:23:09 +0000

    bump. cleanup. version one wooo

--- src/base.js
+++ src/base.js
@@ -4,35 +4,9 @@
  * @module wsc
  */
 var wsc = {};
-wsc.VERSION = '0.14.84';
-wsc.STATE = 'beta';
-wsc.REVISION = '0.14.84';
+wsc.VERSION = '1.0.0';
+wsc.STATE = 'release candidate';
+wsc.REVISION = '0.14.85';
 wsc.defaults = {};
 wsc.defaults.theme = 'wsct_default';
 wsc.defaults.themes = [ 'wsct_default', 'wsct_dAmn' ];
-
-wsc.base = {};
-wsc.base.UI = function( view, options, mozilla, events ) {
-
-    this.LIB = 'base';
-    this.VERSION = '1';
-    this.STATE = 'rc';
-    this.channels = [];
-    
-};
-
-wsc.base.UI.prototype.build = function(  ) {};
-wsc.base.UI.prototype.on = function( event, handler ) {};
-wsc.base.UI.prototype.loop = function(  ) {};
-wsc.base.UI.prototype.create_channel = function( namespace, hidden ) {};
-wsc.base.UI.prototype.remove_channel = function( namespace ) {};
-wsc.base.UI.prototype.channel = function( namespace ) {};
-
-wsc.base.UI.Channel = function( namespace, hidden ) {
-
-    this.namespace = namespace;
-    this.hidden = hidden;
-
-};
-
-wsc.defaults.UI = wsc.base.UI;
