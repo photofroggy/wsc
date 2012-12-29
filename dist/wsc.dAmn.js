bef870e3d87e111f42199c5fca592b046c987061
Henry Rapley <froggywillneverdie@msn.com>
2012-12-29 07:14:51 +0000

    bump.

--- dist/wsc.dAmn.js
+++ dist/wsc.dAmn.js
@@ -4,9 +4,9 @@
  * @module wsc
  */
 var wsc = {};
-wsc.VERSION = '1.1.8';
+wsc.VERSION = '1.1.9';
 wsc.STATE = 'release candidate';
-wsc.REVISION = '0.15.93';
+wsc.REVISION = '0.15.94';
 wsc.defaults = {};
 wsc.defaults.theme = 'wsct_default';
 wsc.defaults.themes = [ 'wsct_default', 'wsct_dAmn' ];
