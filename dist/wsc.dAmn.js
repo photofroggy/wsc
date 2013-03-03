a1daa4b2f0f5cb3d1cd712511436f7dc1fcf6047
Henry Rapley <froggywillneverdie@msn.com>
2013-03-03 03:26:37 +0000

    moved things around a bit

--- dist/wsc.dAmn.js
+++ dist/wsc.dAmn.js
@@ -10239,10 +10239,26 @@ wsc.dAmn.avatar.ext = [ 'gif', 'gif', 'jpg', 'png' ];
  * @constructor
  */
 wsc.dAmn.avatar.link = function( un, icon ) {
+    
+    var src = wsc.dAmn.avatar.src( un, icon );
+    
+    return '<a target="_blank" title=":icon'+un+':" href="http://'+un+'.deviantart.com/"><img class="avatar"\
+            alt=":icon'+un+':" src="' + src + '" height="50" /></a>';
+};
+
+
+/**
+ * Works out the src url for an avatar.
+ *
+ * @method src
+ */
+wsc.dAmn.avatar.src = function( un, icon ) {
+
     icon = parseInt(icon);
     var cachebuster = (icon >> 2) & 15;
     icon = icon & 3;
     var ext = wsc.dAmn.avatar.ext[icon] || 'gif';
+    var ico = 'default';
     
     if (cachebuster) {
         cachebuster = '?' + cachebuster;
@@ -10251,9 +10267,7 @@ wsc.dAmn.avatar.link = function( un, icon ) {
         cachebuster = '';
     }
     
-    if( icon == 0 ) { 
-        ico = 'default';
-    } else {
+    if( icon != 0 ) {
         var ru = new RegExp('\\$un(\\[([0-9]+)\\])', 'g');
         
         var ico = '$un[0]/$un[1]/{un}'.replace(ru, function ( m, s, i ) {
@@ -10262,10 +10276,8 @@ wsc.dAmn.avatar.link = function( un, icon ) {
         ico = replaceAll( ico, '{un}', un.toLowerCase() );
     }
     
-    return '<a target="_blank" title=":icon'+un+':" href="http://'+un+'.deviantart.com/"><img class="avatar"\
-            alt=":icon'+un+':" src="http://a.deviantart.net/avatars/'+ico+'.'+ext+cachebuster+'" height="50" width="50" /></a>';
+    return 'http://a.deviantart.net/avatars/' + ico + '.' + ext + cachebuster;
 };
-
 wsc.dAmn.BDS.Link = function( client, storage, settings ) {
 
     var init = function(  ) {
