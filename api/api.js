5ca9ea30c171ce5ad62192ded24e64431d53cdaa
Henry Rapley <froggywillneverdie@msn.com>
2013-03-29 19:53:32 +0000

    adding first bits of api docs.

---
+++ api/api.js
@@ -0,0 +1,55 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Channel",
+        "Chatbook",
+        "Check",
+        "Client",
+        "Colour",
+        "Control",
+        "Extension",
+        "Field",
+        "Flow",
+        "Form",
+        "Items",
+        "Navigation",
+        "Popup",
+        "Protocol",
+        "Radio",
+        "Settings",
+        "Settings.Config",
+        "Settings.Item",
+        "Settings.Page",
+        "SocketIO",
+        "TablumpString",
+        "Transport",
+        "UI",
+        "WebSocket",
+        "avatar_link",
+        "settings",
+        "template"
+    ],
+    "modules": [
+        "Chatterbox",
+        "dAmn",
+        "wsc"
+    ],
+    "allModules": [
+        {
+            "displayName": "Chatterbox",
+            "name": "Chatterbox",
+            "description": "This is an alternate thing for the UI module.\nChatterbox is basically a thing."
+        },
+        {
+            "displayName": "dAmn",
+            "name": "dAmn",
+            "description": "dAmn module lolol."
+        },
+        {
+            "displayName": "wsc",
+            "name": "wsc",
+            "description": "WebSocket Chat client module."
+        }
+    ]
+} };
+});
