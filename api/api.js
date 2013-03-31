55eca8e90b88c69d4fa42945523ee28173756a9a
Henry Rapley <froggywillneverdie@msn.com>
2013-03-31 17:49:49 +0100

    more docs

--- api/api.js
+++ api/api.js
@@ -1,12 +1,32 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Chatterbox",
+        "Chatterbox.Channel",
+        "Chatterbox.Chatbook",
+        "Chatterbox.Control",
+        "Chatterbox.Navigation",
+        "Chatterbox.Pager",
+        "Chatterbox.Popup",
+        "Chatterbox.Settings",
+        "Chatterbox.Settings.Config",
+        "Chatterbox.Settings.Item",
+        "Chatterbox.Settings.Item.Checkbox",
+        "Chatterbox.Settings.Item.Form",
+        "Chatterbox.Settings.Item.Form.Checkbox",
+        "Chatterbox.Settings.Item.Form.Colour",
+        "Chatterbox.Settings.Item.Form.Field",
+        "Chatterbox.Settings.Item.Form.Radio",
+        "Chatterbox.Settings.Item.Items",
+        "Chatterbox.Settings.Item.Radio",
+        "Chatterbox.Settings.Page",
         "EventEmitter",
         "dAmn.Extension",
         "dAmn.Stash",
         "dAmn.TablumpParser",
         "dAmn.TablumpString",
         "dAmn.avatar",
+        "template",
         "wsc.Channel",
         "wsc.Client",
         "wsc.Control",
@@ -22,10 +42,16 @@ YUI.add("yuidoc-meta", function(Y) {
         "wsc.defaults.Extension"
     ],
     "modules": [
+        "Chatterbox",
         "dAmn",
         "wsc"
     ],
     "allModules": [
+        {
+            "displayName": "Chatterbox",
+            "name": "Chatterbox",
+            "description": "Chatterbox is wsc's default UI library."
+        },
         {
             "displayName": "dAmn",
             "name": "dAmn",
