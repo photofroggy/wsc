4aef63c8cabccf1be79b9bb27040d20482fe0aba
Henry Rapley <froggywillneverdie@msn.com>
2013-04-07 20:31:55 +0100

    bump.

--- dist/wsc.dAmn.js
+++ dist/wsc.dAmn.js
@@ -4,9 +4,9 @@
  * @module wsc
  */
 var wsc = {};
-wsc.VERSION = '1.7.32';
+wsc.VERSION = '1.7.33';
 wsc.STATE = 'release candidate';
-wsc.REVISION = '0.21.117';
+wsc.REVISION = '0.21.118';
 wsc.defaults = {};
 wsc.defaults.theme = 'wsct_dark';
 wsc.defaults.themes = [ 'wsct_dAmn', 'wsct_dark' ];
@@ -4279,7 +4279,7 @@ wsc.Client.prototype.disconnect = function(  ) {
  */
 var Chatterbox = {};
 
-Chatterbox.VERSION = '0.19.83';
+Chatterbox.VERSION = '0.19.84';
 Chatterbox.STATE = 'beta';
 
 /**
