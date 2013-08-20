132cc33025ef1767fa13a656c8f21dfbbf1cc514
Henry Rapley <froggywillneverdie@msn.com>
2013-08-20 21:02:40 +0100

    bug fix. additional event methods on channel object.

--- dist/wsc.js
+++ dist/wsc.js
@@ -5388,6 +5388,8 @@ Chatterbox.BaseTab.prototype.loop = function(  ) {
  */
 Chatterbox.Channel = function( ui, ns, hidden, monitor ) {
     Chatterbox.BaseTab.call( this, ui, ns, hidden, monitor );
+    this.resized = function(  ) {};
+    this.ulbuf = 0;
 };
 
 Chatterbox.Channel.prototype = new Chatterbox.BaseTab;
@@ -5628,7 +5630,7 @@ Chatterbox.Channel.prototype.resize = function( width, height ) {
     // Log panel dimensions
     this.el.l.p.css({
         height: wh - 3,
-        width: cw - 10});
+        width: (cw - 10) - this.ulbuf});
     
     // Scroll again just to make sure.
     this.scroll();
@@ -5648,6 +5650,8 @@ Chatterbox.Channel.prototype.resize = function( width, height ) {
         var tline = (heads[head].m.outerHeight(true) - 5) * (-1);
         heads[head].e.css('top', tline);
     }
+    
+    this.resized();
 };
 
 /**
