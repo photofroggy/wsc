8eeaf7ec5af6513b52828832b9887176c9f6533a
Henry Rapley <froggywillneverdie@msn.com>
2013-11-06 06:07:04 +0000

    bump.

--- dist/wsc.js
+++ dist/wsc.js
@@ -4,9 +4,9 @@
  * @module wsc
  */
 var wsc = {};
-wsc.VERSION = '1.7.45';
+wsc.VERSION = '1.7.46';
 wsc.STATE = 'release candidate';
-wsc.REVISION = '0.21.130';
+wsc.REVISION = '0.21.131';
 wsc.defaults = {};
 wsc.defaults.theme = 'wsct_dark';
 wsc.defaults.themes = [ 'wsct_dAmn', 'wsct_dark' ];
